8ed060421df8c7f7effab5c75875211ac0fb833a
rakshit <rajputraksit571@gmail.com>
2026-09-06 21:24:17 +0530

    added the quote generator

--- app.js
+++ app.js
@@ -18,3 +18,18 @@ add500.addEventListener('click' , ()=>{
     let add = AddWater(500);
     current_water.innerText = `${add/1000}`;
 })
+
+let quoteText = document.getElementById("quote-text");
+let quoteBtn = document.getElementById("new-quote-btn");
+async function getNewQuote(){
+    try{
+        quoteText.innerText = `Loading the quote...`;
+        let response = await fetch('https://dummyjson.com/quotes/random');
+        let data = await response.json();
+        quoteText.innerText = `"${data.quote}" - ${data.author}`;
+    }catch(error){
+        quote.innerText = `Failed to fetch code! Try again`;
+        console.log("API error");
+    }
+}
+quoteBtn.addEventListener('click' , getNewQuote);
